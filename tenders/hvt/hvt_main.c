b3432d712958ecd415b23f5152ccb0b618b6d724
Calascibetta Romain <romain.calascibetta@gmail.com>
2026-08-26 12:32:26 +0200

    Ensure and do our elf_load before to reserve our net-ring

--- tenders/hvt/hvt_main.c
+++ tenders/hvt/hvt_main.c
@@ -262,11 +262,11 @@ int main(int argc, char **argv)
     hvt_mem_size(&mem_size);
     struct hvt *hvt = hvt_init(mem_size);
 
+    if (no_net_ring == 0) hvt_net_reserve_ring(hvt, mft);
     elf_load(elf_fd, elf_filename, hvt->mem, hvt->guest_mem_size,
              HVT_GUEST_MIN_BASE, hvt_guest_mprotect, hvt, &gpa_ep, &gpa_kend);
     close(elf_fd); /* Done with ELF binary */
 
-    if (no_net_ring == 0) hvt_net_reserve_ring(hvt, mft);
     hvt_vcpu_init(hvt, gpa_ep);
 
     setup_modules(hvt, mft);
